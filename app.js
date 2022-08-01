1204b8556e89c99eba70c902d387c597307f5376
tiliax <97850682+tiliax@users.noreply.github.com>
2022-08-01 16:55:14 +0200

    cors enabled

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 let jsonData = require('./pokedex.json')
 const express = require('express') 
 const app = express()
+const cors = require('cors')
 const port = 8000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 //Create a GET route on /pokemon which gives the complete list of pokemon from the JSON
-app.get('/pokemon', (req, res) => {
+app.get('/pokemon',  (req, res) => {
     res.send(jsonData)
   })
 //Create a GET route on /pokemon/:id which gives only one pokemon from the JSON thanks to its id
 app.get('/pokemon/:id', (req, res) => {
-    const pokemonList = jsonData.map((item) => {
-        if(item.id == req.params.id) {
-            //console.log(item)
-            return res.send(item)
-        }else{
-            res.send('Pokemon not found')
-        }
-    })
+    const result = jsonData.find( pokemon => pokemon.id == req.params.id)
+    res.send(result)
   })
 
 
